a648d89e0b9dd70f413c40e1efeaf2de3be677f0
sydocon <78183097+sydocon@users.noreply.github.com>
2021-08-02 13:22:59 -0400

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,66 @@
+# define a basic function
+def func1():
+    print(" i am a function")
+    
+# function that takes arguments
+def func2(arg1, arg2):
+    print(arg1 ," ", arg2)
+
+# function that returns a value
+def cube(x):
+    return x*x*x
+    
+# function with default value for an arguments
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result= result * num
+    return result
+
+
+# function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+
+
+func1()
+print(func1())
+print(func1)
+
+func2(10,20)
+print(func2(10,20))
+
+print(cube(3))
+
+print(power(2))
+print(power(2,3))
+print(power(x=3, num=2))
+
+print(multi_add(4,5,10,4))
+
+
+
+
+
+
+
+# What it prints :
+# i am a function
+# i am a function
+# None
+# <function func1 at 0x7f8f57f5dbf8>
+
+# 10   20
+# 10   20
+# None
+
+# 27
+
+# 2
+# 8
+# 8
+
+# 23
