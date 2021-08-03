91b8e33c857ff0414564f805b3312fd91e258aa9
sydocon <78183097+sydocon@users.noreply.github.com>
2021-08-03 12:00:47 -0400

    Create Exercise6-MathFunctions.py

---
+++ Exercise6-MathFunctions.py
@@ -0,0 +1,16 @@
+# Example file for working with importing and using modules
+
+
+# import the math module,  which contains features for working with math
+import math
+
+# the math module contains lots of pre-build functions
+print("The square root of 16 is ", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants
+print("Pi is ", math.pi)
+
+
+# Prints:
+# The square root of 16 is  4.0
+# Pi is  3.141592653589793
